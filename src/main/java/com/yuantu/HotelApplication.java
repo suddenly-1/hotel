eff75aeef6bcea8013444ad5bfbe1e56dc726c87
dead-bear <1905553267@qq.com>
2020-06-03 17:11:03 +0800

    酒店信息管理及客房录入

--- src/main/java/com/yuantu/HotelApplication.java
+++ src/main/java/com/yuantu/HotelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelProjectApplication {
+public class HotelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HotelProjectApplication.class, args);
+        SpringApplication.run(HotelApplication.class, args);
     }
 
 }
