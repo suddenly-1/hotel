bf898f0d585173b4ea0cbbd1cae6cc252b1927d4
suddenly <2676178516@qq.com>
2020-06-04 11:11:10 +0800

    update

--- src/main/java/com/yuantu/controller/user/AccountController.java
+++ src/main/java/com/yuantu/controller/user/AccountController.java
@@ -10,27 +10,28 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@RequestMapping("/Account")
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
-    @PostMapping("/Account/register")
+    @PostMapping("/register")
     public String register(@RequestBody UserForm userForm){
         return JSON.toJSONString(accountService.register(userForm));
     }
 
-    @GetMapping("/Account/info/{id}")
+    @GetMapping("/info/{id}")
     public String queryUserInfo(@PathVariable int id){
         return JSON.toJSONString(accountService.queryUserInfo(id));
     }
 
-    @PostMapping("/Account/update")
+    @PostMapping("/update")
     public String updateUserInfo(@RequestBody UserInfo userInfo){
         return JSON.toJSONString(accountService.updateUserInfo(userInfo));
     }
 
-    @GetMapping("/Account/queryUserById/{id}")
+    @GetMapping("/queryUserById/{id}")
     public String queryUserById(@PathVariable int id){
         return JSON.toJSONString(accountService.queryUserById(id));
     }
